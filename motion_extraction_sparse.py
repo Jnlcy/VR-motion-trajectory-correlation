3990b533f27b3c943130463e018d82b5add9974c
Jnlcy <jiayizhang01@gmail.com>
2022-11-07 21:56:45 +0000

    quiver

--- motion_extraction_sparse.py
+++ motion_extraction_sparse.py
@@ -2,19 +2,45 @@ from optparse import make_option
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
+'''
 def plotflow(mask,frame,new,old):
     for i, (new, old) in enumerate(zip(new, old)):
         a, b = new.ravel()
         c, d = old.ravel()
         # Green color in BGR
         color = (0, 255, 0)      
-        mask = cv2.arrowedLine(mask, (int(a),int (b)), (int(c),int(d)), color, 2)
+        mask = cv2.arrowedLine(mask, (int(c),int(d)), (int(a),int (b)), color, 2)
         #frame = cv2.circle(frame, (int(a),int(b) ), 5, color, -1)
     img = cv2.add(frame, mask)
     cv2.imshow('output',img)
     return img
 
+'''
+
+def plotquiver(t,frame,new,old):
+    
+    x=[]
+    y=[]
+    dx=[]
+    dy=[]
+
+    for i, (new, old) in enumerate(zip(new, old)):
+        a, b = new.ravel()
+        c, d = old.ravel()
+        x.append(c)
+        y.append(d)
+        dx.append(a-c)
+        dy.append(b-d)
+
+   
+
+    #plot 
+    
+    plt.imshow(frame)
+    plt.quiver(x,y,dx,dy,angles='xy', scale_units='xy', scale=0.5, pivot='mid',color='y')
+    plt.savefig("flow"+str(t)+".jpg")
+
+  
 
 def lucas_kanade_method(video_path):
     cap = cv2.VideoCapture(video_path)
@@ -22,7 +48,7 @@ def lucas_kanade_method(video_path):
     
     color = (0, 255, 0) 
     
-    feature_params = dict(maxCorners=100, qualityLevel=0.2, minDistance=7, blockSize=7)
+    feature_params = dict(maxCorners=100, qualityLevel=0.3, minDistance=7, blockSize=7)
     # Parameters for lucas kanade optical flow
 
     lk_params = dict(
@@ -46,10 +72,10 @@ def lucas_kanade_method(video_path):
         print(t)
 
         #create old frame
-        cap.set(cv2.CAP_PROP_POS_MSEC, float((t-5*dt)*MILLISECONDS))
+        cap.set(cv2.CAP_PROP_POS_MSEC, float((t-4*dt)*MILLISECONDS))
         ret, old_frame = cap.read()
         
-
+        #convert the frame into grey scale
         old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
         p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
         mask = np.zeros_like(old_frame)
@@ -88,10 +114,11 @@ def lucas_kanade_method(video_path):
             good_old = p0[st == 1]
         
             #plot optical flow
-            img=plotflow(mask,frame,good_new,good_old)
+            #img=plotflow(mask,frame,good_new,good_old)
+            img=plotquiver(t,frame,good_new,good_old)
             k = cv2.waitKey(25) & 0xFF
             
-            cv2.imwrite("flow"+str(t)+".jpg", img) 
+            #cv2.imwrite("flow"+str(t)+".jpg", img) 
             
       
 
