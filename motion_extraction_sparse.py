283b7bdd6e8471665db91d1c8c2848a7371e9458
Jnlcy <jiayizhang01@gmail.com>
2022-11-07 16:08:57 +0000

    plot

--- motion_extraction_sparse.py
+++ motion_extraction_sparse.py
@@ -1,96 +1,106 @@
-
-
-
-
 from optparse import make_option
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-def save_optical_flow(flow):
-    videoname='people'
-    np.savetxt('flow.csv',flow,delimiter=",")
+def plotflow(mask,frame,new,old):
+    for i, (new, old) in enumerate(zip(new, old)):
+        a, b = new.ravel()
+        c, d = old.ravel()
+        # Green color in BGR
+        color = (0, 255, 0)      
+        mask = cv2.arrowedLine(mask, (int(a),int (b)), (int(c),int(d)), color, 2)
+        #frame = cv2.circle(frame, (int(a),int(b) ), 5, color, -1)
+    img = cv2.add(frame, mask)
+    cv2.imshow('output',img)
+    return img
 
 
-
-        
-    
 def lucas_kanade_method(video_path):
     cap = cv2.VideoCapture(video_path)
     # params for ShiTomasi corner detection
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    timestamps = [cap.get(cv2.CAP_PROP_POS_MSEC)]
-    calc_timestamps = [0.0]
-
+    
+    color = (0, 255, 0) 
     
     feature_params = dict(maxCorners=100, qualityLevel=0.3, minDistance=7, blockSize=7)
     # Parameters for lucas kanade optical flow
+
     lk_params = dict(
         winSize=(15, 15),
         maxLevel=2,
         criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03),
     )
-    # Create some random colors
-    color = np.random.randint(0, 255, (100, 3))
+   
     # Take first frame and find corners in it
     ret, old_frame = cap.read()
     old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
     p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
-    # Create a mask image for drawing purposes
+    # Create a mask image for plotting purposes
     mask = np.zeros_like(old_frame)
-    optical_flow=[]
     
-    idx = 0
-    while True:
-        
+    
+    for t in [0.2,0.5,1,3]:
+        MILLISECONDS = 1000
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        dt =1/fps
+        print(t)
+
+        #create old frame
+        cap.set(cv2.CAP_PROP_POS_MSEC, float((t-dt)*MILLISECONDS))
+        ret, old_frame = cap.read()
+        #save frame
+        cv2.imwrite('frame'+str(t)+'.jpg',old_frame)
+
+        old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+        p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
+        mask = np.zeros_like(old_frame)
+
+
+        cap.set(cv2.CAP_PROP_POS_MSEC, float(t*MILLISECONDS))
         ret, frame = cap.read()
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+
         if not ret:
             break
-        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
         # forward-backwoard error detection
         p1, st, err = cv2.calcOpticalFlowPyrLK(
             old_gray, frame_gray, p0, None, **lk_params
         )
         
-        
+
         # If no flow, look for new points
         if p1 is None:
 
             old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-
             p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
-
             # Get rid of old lines
             mask = np.zeros_like(old_frame)
+            cv2.imshow ('frame', frame)
 
-            cv2.imshow('frame', frame)
         else:
         # Select good points
-            time = cap.get(cv2.CAP_PROP_POS_MSEC)
+            
             good_new = p1[st == 1]
             good_old = p0[st == 1]
-            # draw the tracks
-            
-            for i, (new, old) in enumerate(zip(good_new, good_old)):
-                a, b = new.ravel()
-                c, d = old.ravel()
-                optical_flow.append([int(idx),time,a,b])
-                mask = cv2.line(mask, (int(a),int (b)), (int(c),int(d)), color[i].tolist(), 2)
-                frame = cv2.circle(frame, (int(a),int(b) ), 5, color[i].tolist(), -1)
-            img = cv2.add(frame, mask)
-            cv2.imshow('output',img)
-            
-            k = cv2.waitKey(25) & 0xFF
+        
+            #plot optical flow
+            img=plotflow(mask,frame,good_new,good_old)
+
+            '''
             if k == 27:
                 break
             if k == ord("c"):
                 mask = np.zeros_like(old_frame)
-            # Now update the previous frame and previous points
-            old_gray = frame_gray.copy()
-            p0 = good_new.reshape(-1, 1, 2)
+            '''
+            k = cv2.waitKey(25) & 0xFF
+            
+            cv2.imwrite("flow"+str(t)+".jpg", img) 
+            
+      
 
-        idx +=1
-    cv2.imwrite("frame.jpg", img)    
-    return optical_flow
     #save_optical_flow(flow)
     
     cap.release()
@@ -99,8 +109,7 @@ def main():
     path = 'people.mp4'
     
     flow=lucas_kanade_method(path)
-    print(flow)
-    save_optical_flow(flow)
+    
     
 
     
