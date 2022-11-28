e88f8c9ed70870e1901c5c9d76b717a35c453f79
Jnlcy <jiayizhang01@gmail.com>
2022-11-28 16:07:20 +0000

    get corner

--- David_MMSys_18/viewport_extraction.py
+++ David_MMSys_18/viewport_extraction.py
@@ -2,11 +2,28 @@
 import pandas as pd
 import numpy as np
 from spherical_geometry import polygon
+from Utils import *
+from nfov import*
 
 HOR_DIST = degrees_to_radian(110)
 HOR_MARGIN = degrees_to_radian(110 / 2)
 VER_MARGIN = degrees_to_radian(90 / 2)
+_fov_points = dict()
+_fov_polys = dict()
+X1Y0Z0 = np.array([1, 0, 0])
 
+_fov_x1y0z0_fov_points_euler = np.array([
+    eulerian_in_range(-HOR_MARGIN, VER_MARGIN),
+    eulerian_in_range(HOR_MARGIN, VER_MARGIN),
+    eulerian_in_range(HOR_MARGIN, -VER_MARGIN),
+    eulerian_in_range(-HOR_MARGIN, -VER_MARGIN)
+])
+_fov_x1y0z0_points = np.array([
+    eulerian_to_cartesian(*_fov_x1y0z0_fov_points_euler[0]),
+    eulerian_to_cartesian(*_fov_x1y0z0_fov_points_euler[1]),
+    eulerian_to_cartesian(*_fov_x1y0z0_fov_points_euler[2]),
+    eulerian_to_cartesian(*_fov_x1y0z0_fov_points_euler[3])
+])
 
 
 #david.create_and_store_sampled_dataset()
@@ -17,7 +34,6 @@ def load_data():
     # df with (dataset, user, video, times, traces)
                 # times has only time-stamps
                 # traces has only x, y, z (in 3d coordinates)
-    print(dataset)
     data = [('david',
             'david' + '_' + user,
             'david' + '_' + video,
@@ -29,8 +45,27 @@ def load_data():
                     'traces'])
     return tmpdf
 #get xyz corner of the view port
-def get_xyz():
-    ds = load_data()
+
+def get_one_trace() -> np.array:
+    dataset = load_data()
+    return dataset.iloc[0]['traces'][0]
+print(get_one_trace())
+
+def xyz_xy(x,y,z):
+    u= x/z
+    v=y/z
+
+def get_corner(img):
+    trace = get_one_trace()
+    trace = [trace[1],trace[2],trace[3]]
+    center_point = xyz_xy(trace[1],trace[2],trace[3])
+    nfov.toNFOV(img, center_point)
+
+    
+
+
+
+
 
 
 
